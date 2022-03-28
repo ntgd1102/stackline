eb644a763d24605e4facdbca89da1b4fca61c07b
Tao Ning <tao.ning>
2022-03-27 23:13:29 -0700

    style polish

--- my-app/src/containers/ProductInfoContainer.tsx
+++ my-app/src/containers/ProductInfoContainer.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import _ from 'lodash';
 import { useSelector } from 'react-redux';
 import './ProductInfoContainer.css'
 import {getRetailProductInfo} from '../store/selectors';
 
 const ProductInfoContainer: React.FC = () => {
     const retailProductInfo = useSelector(getRetailProductInfo);
+    if (_.isEmpty(retailProductInfo)) {
+        return null;
+    }
+
     return (
         <div className='product-info-container'>
-            <img
+            {retailProductInfo.image && <img
                 className="image"
                 src={retailProductInfo.image}
                 alt="retail item"
-            />
+            />}
             <h4>{retailProductInfo.title}</h4>
             <span className="subtitle">{retailProductInfo?.subtitle}</span>
             <div className="tags-container">
